a50a8d694d0de962b8b1d80417c36d8c91064df9
Graham Ollis <plicease@cpan.org>
2016-01-03 11:26:54 -0500

    simplify

--- lib/Math.pm
+++ lib/Math.pm
@@ -1,6 +1,5 @@
 use strict;
 use warnings;
-use experimental qw( signatures );
 
 package Math {
 
@@ -8,28 +7,11 @@ package Math {
   use Clustericious::RouteBuilder;
   use Clustericious::Log;
 
-  # ABSTRACT: For testing only
-  # VERSION
-
   get '/add/:a/:b' => sub {
     my($c) = @_;
     $c->render(autodata => [$c->param('a') + $c->param('b')]);
   };
 
-  #sub _fib ($n)
-  #{
-  #  return undef if $n < 0;
-  #  return 0 if $n == 0;
-  #  return 1 if $n == 1 || $n == 2;
-  #  return _fib($n-1) + _fib($n-2);
-  #}
-  #
-  #get '/fib/:n' => sub {
-  #  my($c) = @_;
-  #  my $n = $c->param('n');
-  #  $c->render(autodata => [ _fib($n) ] );
-  #};
-  
   get '/fib/:n' => sub {
     my($c) = @_;
     my $n = $c->param('n');
