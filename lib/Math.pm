3b6c929674461758cf02cffa925b6793c21873e3
Graham Ollis <plicease@cpan.org>
2016-01-03 08:52:40 -0500

    starter

--- lib/Math.pm
+++ lib/Math.pm
@@ -1,9 +1,49 @@
-package Math;
-
 use strict;
 use warnings;
+use experimental qw( signatures );
 
-# ABSTRACT: For testing only
-# VERSION
+package Math {
+
+  use base qw( Clustericious::App);
+  use Clustericious::RouteBuilder;
+  use Clustericious::Log;
+
+  # ABSTRACT: For testing only
+  # VERSION
+
+  get '/add/:a/:b' => sub {
+    my($c) = @_;
+    $c->render(autodata => [$c->param('a') + $c->param('b')]);
+  };
+
+  sub _fib ($n)
+  {
+    return undef if $n < 0;
+    return 0 if $n == 0;
+    return 1 if $n == 1 || $n == 2;
+    return _fib($n-1) + _fib($n-2);
+  }
+
+  get '/fib/:n' => sub {
+    my($c) = @_;
+    $c->tx->res->headers->{n} = $c->param('n');
+    $c->render(autodata => [ _fib($c->param('n')) ] );
+  };
+
+}
+
+package Math::Client {
+
+  use Mojo::URL;
+  use base qw( Clustericious::Client );
+
+  sub fib ($self, $n)
+  {
+    my $url = Mojo::URL->new($self->config->url);
+    $url->path("/fib/@{[ $n ]}");
+    $self->ua->get($url)->res->json->[0];
+  }
+
+}
 
 1;
---
+++ t/basic.t
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::Clustericious::Cluster;
+
+my $cluster = Test::Clustericious::Cluster->new;
+$cluster->create_cluster_ok('Math');
+my $t = $cluster->t;
+
+$t->get_ok('/add/1/2')
+  ->status_is(200)
+  ->json_is('/0', 3);
+
+$t->get_ok('/fib/6')
+  ->status_is(200)
+  ->json_is('/0', 8);
+
+my $client = Math::Client->new;
+
+is $client->fib(6), 8;
+
+done_testing;
+
+__DATA__
+
+@@ etc/Math.conf
+---
+url: <%= cluster->url %>
